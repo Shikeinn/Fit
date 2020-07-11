186b46c1a7e37815d55f364c7f442e49c10d193a
masturffect <ianseal10@gamil.com>
2020-07-11 10:39:27 -0400

    pycache files update from runserver

--- Fit/views.py
+++ Fit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from Fit.forms import UserForm
 from Fit.models import User
 # Create your views here.
 
