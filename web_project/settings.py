023a4edb04a1497820cc0be8b1ab444b805e1794
masturffect <ianseal10@gamil.com>
2020-07-11 00:40:38 -0400

    creating login page

--- web_project/settings.py
+++ web_project/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'web_project.urls'
 
+LOGIN_REDIRECT_URL = '/'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
