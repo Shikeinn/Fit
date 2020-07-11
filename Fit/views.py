422485f3a78231ced081a7a6704a5ff75f94f58a
Sam Hollingsworth <samogh88@gmail.com>
2020-07-10 22:47:26 -0500

    db User setup

--- Fit/views.py
+++ Fit/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView
+from Fit.models import User
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    def get(self, request):
+        bruh = "Hello World #swagmoney #cashhaver #dollarowner"
+        user = User.objects.all()
+        args = {'user': user}
+        return render(request, self.template_name, args)
