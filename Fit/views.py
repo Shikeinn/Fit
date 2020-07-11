877e227270d46801a0a0a8586158825d4b6a6736
CodyManess <cody.maness@outlook.com>
2020-07-11 08:56:02 -0500

    Added login.html

--- Fit/views.py
+++ Fit/views.py
@@ -7,18 +7,18 @@ from Fit.forms import UserForm
 
 class HomeView(TemplateView):
     template_name = "home.html"
-    def get(self, request):
-        form = UserForm() #initialize form
-        args = {'form': form}
-        User.objects.all().delete() #clears data from last session
-        return render(request, self.template_name, args)
-    def post(self, request):
-        form = UserForm(request.POST) #grabs form from POST request
-        if (form.is_valid()):
-            first_name = form.cleaned_data['first_name'] #cleaned_data gives SQL injection protection
-            last_name = form.cleaned_data['last_name']
-            p = User(first_name = first_name, last_name = last_name ) #create newe User object
-            p.save() #push new object to User model
-            name_list = User.objects.all() #pulls all data from the User model
-            args = {'form': form, 'first_name':first_name, 'last_name':last_name, 'list':name_list}
-        return render(request, self.template_name, args)
+    # def get(self, request):
+    #     form = UserForm() #initialize form
+    #     args = {'form': form}
+    #     User.objects.all().delete() #clears data from last session
+    #     return render(request, self.template_name, args)
+    # def post(self, request):
+    #     form = UserForm(request.POST) #grabs form from POST request
+    #     if (form.is_valid()):
+    #         first_name = form.cleaned_data['first_name'] #cleaned_data gives SQL injection protection
+    #         last_name = form.cleaned_data['last_name']
+    #         p = User(first_name = first_name, last_name = last_name ) #create newe User object
+    #         p.save() #push new object to User model
+    #         name_list = User.objects.all() #pulls all data from the User model
+    #         args = {'form': form, 'first_name':first_name, 'last_name':last_name, 'list':name_list}
+    #     return render(request, self.template_name, args)
